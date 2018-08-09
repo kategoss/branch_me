93cedb06395132327c3ed78e528911e132053c82
Kate Goss and Liza Tennis <liza@>
2018-08-09 10:29:23 -0700

    link animal inputs and add sibling features

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 
-  $("#formOne").click(function() {
+  $("#formOne").click(function(event) {
   var animalInput = $("input:radio[name=animal]:checked").val();
 
-
-  $("#animalfacts").show();
+if(animalInput === "turtles") {
+  $("#turtles").toggle();
+  $("#turtles").siblings().hide();
+} else if (animalInput === "snakes") {
+  $("#snakes").toggle();
+  $("#snakes").siblings().hide();
+} else if (animalInput === "monkeys") {
+  $("#monkeys").toggle();
+  $("#monkeys").siblings().hide();
+}
 
   event.preventDefault();
   });
